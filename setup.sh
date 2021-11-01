a40e673ed3765944400fa6925b4ba21cbd7324eb
ksyasuda <ksyasuda@umich.edu>
2021-11-01 00:23:51 -0700

    change rofi menu to have 12 lines by default
    no longer run count query on database and set lines based off the count

--- setup.sh
+++ setup.sh
@@ -4,15 +4,17 @@
 # As well as set up the history database
 # DO NOT RUN AS ROOT
 
-log () {
+log() {
     printf "%s\n" "$1"
 }
 
 # DIR="$HOME/.ani-cli/"
 DB="history.sqlite3"
 DIR="$XDG_CONFIG_HOME/ani-cli/"
+MPV_DIR="$XDG_CONFIG_HOME/mpv/"
 
 [[ -z "$XDG_CONFIG_HOME" ]] && DIR="$HOME/.ani-cli"
+[[ -z "$XDG_CONFIG_HOME" ]] && MPV_DIR="$HOME/.config/.ani-cli"
 
 printf "%s\n" "INSTALL DIR: $DIR"
 
@@ -22,7 +24,7 @@ if [[ "$DIR" == "$HOME/.config" ]]; then
 fi
 
 log "Deleting old directory..."
-# rm -rf "$DIR"
+rm -rf "$DIR"
 log "Directory deleted..."
 
 log "Creating directory $DIR"
@@ -30,8 +32,8 @@ mkdir -p "$DIR"
 log "Directory created"
 
 log "Creating history database..."
-sqlite3 "$DIR"/"$DB" < sql/watch_history_tbl.sql
-sqlite3 "$DIR"/"$DB" < sql/search_history_tbl.sql
+sqlite3 "$DIR"/"$DB" <sql/watch_history_tbl.sql
+sqlite3 "$DIR"/"$DB" <sql/search_history_tbl.sql
 log "History database created..."
 
 log "Moving theme files..."
@@ -39,3 +41,11 @@ cp themes/meh.rasi "$DIR"/
 cp themes/arc_dark_transparent_colors.rasi "$DIR"/
 log "Theme files moved..."
 
+log "Creating mpv/scripts/ directory if it doesn't exist..."
+mkdir -p "$MPV_DIR/scripts/"
+log "Created mpv scripts directory..."
+log "Moving skip-intro.lua into mpv scripts directory..."
+cp skip-intro.lua "$MPV_DIR/scripts/skip-intro.lua"
+log "Moved skip-intro.lua into scripts directory..."
+
+log "Setup Complete...."
