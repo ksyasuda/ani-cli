7c0f505231dd0332b402b87f67fc7287571edc9e
ksyasuda <ksyasuda@umich.edu>
2021-11-07 22:02:31 -0800

    update rofi config file and add db cleanup option

--- db.sh
+++ db.sh
@@ -7,39 +7,48 @@ log() {
 	printf "%s\n" "$1"
 }
 
-while getopts 'cdrq' OPT; do
+while getopts 'cdrqC' OPT; do
 	case "$OPT" in
-	c)
-		log "Creating database..."
-		sqlite3 "$DIR"/"$DB" <sql/search_history_tbl.sql
-		sqlite3 "$DIR"/"$DB" <sql/watch_history_tbl.sql
-		log "Created database..."
-		;;
-	d)
-		log "Deleting database..."
-		echo "$DIR/$DB"
-		rm "$DIR/$DB"
-		log "Database deleted..."
-		;;
+		c)
+			log "Creating database..."
+			sqlite3 "$DIR"/"$DB" <sql/search_history_tbl.sql
+			sqlite3 "$DIR"/"$DB" <sql/watch_history_tbl.sql
+			log "Created database..."
+			;;
+		d)
+			log "Deleting database..."
+			echo "$DIR/$DB"
+			rm "$DIR/$DB"
+			log "Database deleted..."
+			;;
 
-	r)
-		log "Deleting database..."
-		rm -rf "$DIR"/"$DB"
-		mkdir -p "$DIR"
-		log "Database deleted..."
-		log "Creating database..."
-		sqlite3 "$DIR"/"$DB" <sql/search_history_tbl.sql
-		sqlite3 "$DIR"/"$DB" <sql/watch_history_tbl.sql
-		log "Created database..."
-		;;
-	q)
-		log "Connecting to database..."
-		sqlite3 "$DIR/$DB"
-		log "Ending connection to databse..."
-		;;
-	*)
-		log "Don't get here"
-		exit 1
-		;;
+		r)
+			log "Deleting database..."
+			rm -rf "$DIR"/"$DB"
+			mkdir -p "$DIR"
+			log "Database deleted..."
+			log "Creating database..."
+			sqlite3 "$DIR"/"$DB" <sql/search_history_tbl.sql
+			sqlite3 "$DIR"/"$DB" <sql/watch_history_tbl.sql
+			log "Created database..."
+			;;
+		q)
+			log "Connecting to database..."
+			sqlite3 "$DIR/$DB"
+			log "Ending connection to databse..."
+			;;
+		C)
+			log "Cleaning up database..."
+			stmt="DELETE FROM search_history WHERE anime_name IS NULL or anime_name = ''"
+			log "Cleaning up search history..."
+			sqlite3 "$DIR/$DB" <<<"$stmt"
+			stmt="DELETE FROM watch_history WHERE anime_name IS NULL or anime_name = ''"
+			log "Cleaning up watch history..."
+			sqlite3 "$DIR/$DB" <<<"$stmt"
+			;;
+		*)
+			log "Does not exist or not implemented yet..."
+			exit 1
+			;;
 	esac
 done
