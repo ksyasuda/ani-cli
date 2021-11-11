4853dea3d533c1b02d018a286e1c774a69efd412
ksyasuda <ksyasuda@umich.edu>
2021-11-11 14:13:11 -0800

    add sync database option
    Added option to sync the history databases for the same user
    across devices on the same network (or the internet if that is set up on
    the machines)

--- db.sh
+++ db.sh
@@ -9,7 +9,46 @@ log() {
 	printf "%s\n" "$1"
 }
 
-while getopts 'cdrqC' OPT; do
+sync_search_history() {
+	cnt=0
+	temp_db="/tmp/aniwrapper_tmp_history.sqlite3"
+	while read -r line; do
+		anime_name=$(awk -F '|' '{print $2}' <<<"$line")
+		res=$(sqlite3 -noheader "$DIR/$DB" <<<"SELECT anime_name FROM search_history WHERE anime_name = '$anime_name'")
+		if [[ "${res/ //}" == "" ]]; then
+			log "Adding $line to search history..."
+			search_date=$(awk -F '|' '{print $3}' <<<"$line")
+			sqlite3 "$DIR/$DB" <<<"INSERT INTO search_history(anime_name, search_date) VALUES('$anime_name', '$search_date')"
+			if [[ "$?" -ne 0 ]]; then
+				log "Error inserting row $line"
+			fi
+			((++cnt))
+		fi
+	done <<<"$(sqlite3 -noheader "$temp_db" <<<"SELECT DISTINCT * FROM search_history")"
+	log "Inserted $cnt rows into search_history table"
+}
+
+sync_watch_history() {
+	cnt=0
+	temp_db="/tmp/aniwrapper_tmp_history.sqlite3"
+	while read -r line; do
+		anime_name=$(awk -F '|' '{print $2}' <<<"$line")
+		res=$(sqlite3 -noheader "$DIR/$DB" <<<"SELECT anime_name FROM watch_history WHERE anime_name = '$anime_name'")
+		if [[ "${res/ //}" == "" ]]; then
+			log "Adding $line to watch history..."
+			episode_num=$(awk -F '|' '{print $3}' <<<"$line")
+			watch_date=$(awk -F '|' '{print $NF}' <<<"$line")
+			sqlite3 "$DIR/$DB" <<<"INSERT INTO watch_history(anime_name, episode_number, watch_date) VALUES('$anime_name', '$episode_num', '$watch_date')"
+			if [[ "$?" -ne 0 ]]; then
+				log "Error inserting row $line"
+			fi
+			((++cnt))
+		fi
+	done <<<"$(sqlite3 -noheader "$temp_db" <<<"SELECT DISTINCT * FROM watch_history")"
+	log "Inserted $cnt rows into watch_history table"
+}
+
+while getopts 'cdrqCs' OPT; do
 	case "$OPT" in
 		c)
 			log "Creating database..."
@@ -47,6 +86,29 @@ while getopts 'cdrqC' OPT; do
 			stmt="DELETE FROM watch_history WHERE anime_name IS NULL or anime_name = ''"
 			log "Cleaning up watch history..."
 			sqlite3 "$DIR/$DB" <<<"$stmt"
+			;;
+		s)
+			printf "%s" "Enter connection string for remote user in the form user@host: "
+			read -r connection_str
+			printf "%s" "Enter port to connect to remote host with or leave blank for default (22): "
+			read -r port
+			if [[ ! "${port/ //}" == "" ]]; then
+				PORT="$port"
+			fi
+
+			if [[ ! "@" != *"$connection_str" ]]; then
+				log "Enter in full connection string to remote user in the form: user@host"
+				exit 1
+			fi
+
+			log "Syncing database with: $connection_str on port $PORT"
+
+			scp -P "$PORT" "$connection_str:$DIR/$DB" "$temp_db"
+			if [[ "$?" -ne 0 ]]; then
+				printf "%s\n" "Error getting database file from remote host"
+				exit 1
+			fi
+			sync_search_history && sync_watch_history
 			;;
 		*)
 			log "Does not exist or not implemented yet..."
