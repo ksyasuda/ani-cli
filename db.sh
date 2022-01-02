55470e87e97ecae1e27f66b96b6eb0a90837e5e4
ksyasuda <ksyasuda@umich.edu>
2022-01-02 09:53:16 -0800

    update code formatting

--- db.sh
+++ db.sh
@@ -9,51 +9,12 @@ log() {
 	printf "%s\n" "$1"
 }
 
-sync_search_history() {
-	cnt=0
-	temp_db="/tmp/aniwrapper_tmp_history.sqlite3"
-	while read -r line; do
-		anime_name=$(awk -F '|' '{print $2}' <<<"$line")
-		res=$(sqlite3 -noheader "$DIR/$DB" <<<"SELECT anime_name FROM search_history WHERE anime_name = '$anime_name'")
-		if [[ "${res/ //}" == "" ]]; then
-			log "Adding $line to search history..."
-			search_date=$(awk -F '|' '{print $3}' <<<"$line")
-			sqlite3 "$DIR/$DB" <<<"INSERT INTO search_history(anime_name, search_date) VALUES('$anime_name', '$search_date')"
-			if [[ "$?" -ne 0 ]]; then
-				log "Error inserting row $line"
-			fi
-			((++cnt))
-		fi
-	done <<<"$(sqlite3 -noheader "$temp_db" <<<"SELECT DISTINCT * FROM search_history")"
-	log "Inserted $cnt rows into search_history table"
-}
-
-sync_watch_history() {
-	cnt=0
-	temp_db="/tmp/aniwrapper_tmp_history.sqlite3"
-	while read -r line; do
-		anime_name=$(awk -F '|' '{print $2}' <<<"$line")
-		res=$(sqlite3 -noheader "$DIR/$DB" <<<"SELECT anime_name FROM watch_history WHERE anime_name = '$anime_name'")
-		if [[ "${res/ //}" == "" ]]; then
-			log "Adding $line to watch history..."
-			episode_num=$(awk -F '|' '{print $3}' <<<"$line")
-			watch_date=$(awk -F '|' '{print $NF}' <<<"$line")
-			sqlite3 "$DIR/$DB" <<<"INSERT INTO watch_history(anime_name, episode_number, watch_date) VALUES('$anime_name', '$episode_num', '$watch_date')"
-			if [[ "$?" -ne 0 ]]; then
-				log "Error inserting row $line"
-			fi
-			((++cnt))
-		fi
-	done <<<"$(sqlite3 -noheader "$temp_db" <<<"SELECT DISTINCT * FROM watch_history")"
-	log "Inserted $cnt rows into watch_history table"
-}
-
 while getopts 'cdrqCs' OPT; do
 	case "$OPT" in
 		c)
 			log "Creating database..."
-			sqlite3 "$DIR"/"$DB" <sql/search_history_tbl.sql
-			sqlite3 "$DIR"/"$DB" <sql/watch_history_tbl.sql
+			sqlite3 "$DIR"/"$DB" < sql/search_history_tbl.sql
+			sqlite3 "$DIR"/"$DB" < sql/watch_history_tbl.sql
 			log "Created database..."
 			;;
 		d)
@@ -69,8 +30,8 @@ while getopts 'cdrqCs' OPT; do
 			mkdir -p "$DIR"
 			log "Database deleted..."
 			log "Creating database..."
-			sqlite3 "$DIR"/"$DB" <sql/search_history_tbl.sql
-			sqlite3 "$DIR"/"$DB" <sql/watch_history_tbl.sql
+			sqlite3 "$DIR"/"$DB" < sql/search_history_tbl.sql
+			sqlite3 "$DIR"/"$DB" < sql/watch_history_tbl.sql
 			log "Created database..."
 			;;
 		q)
@@ -82,10 +43,10 @@ while getopts 'cdrqCs' OPT; do
 			log "Cleaning up database..."
 			stmt="DELETE FROM search_history WHERE anime_name IS NULL OR anime_name = ''"
 			log "Cleaning up search history..."
-			sqlite3 "$DIR/$DB" <<<"$stmt"
+			sqlite3 "$DIR/$DB" <<< "$stmt"
 			stmt="DELETE FROM watch_history WHERE anime_name IS NULL OR anime_name = '' OR episode_number = 0"
 			log "Cleaning up watch history..."
-			sqlite3 "$DIR/$DB" <<<"$stmt"
+			sqlite3 "$DIR/$DB" <<< "$stmt"
 			;;
 		s)
 			printf "%s" "Enter connection string for remote user in the form user@host: "
--- setup.sh
+++ setup.sh
@@ -11,16 +11,16 @@ DB="history.sqlite3"
 
 # Set the XDG_CONFIG_HOME locally if not set
 if [[ -z "$XDG_CONFIG_HOME" ]]; then
-    XDG_CONFIG_HOME="$HOME/.config"
+	XDG_CONFIG_HOME="$HOME/.config"
 fi
 
 DIR="$XDG_CONFIG_HOME/aniwrapper"
 MPV_DIR="$XDG_CONFIG_HOME/mpv"
 
 log() {
-    if [[ "$VERBOSE" -eq 1 ]]; then
-        printf "%s\n" "$*"
-    fi
+	if [[ "$VERBOSE" -eq 1 ]]; then
+		printf "%s\n" "$*"
+	fi
 }
 
 # executes aniwrapper setup
@@ -30,52 +30,52 @@ log() {
 # 4. move skip-intro.lua into mpv/scripts folder
 # 5. move the aniwrapper icon to $XDG_CONFIG_HOME/aniwrapper/ directory
 run_setup() {
-    log "INSTALL DIR: $DIR"
+	log "INSTALL DIR: $DIR"
 
-    if [[ ! -d "$DIR" ]]; then
-        log "Creating directory $DIR"
-        mkdir -p "$DIR"
-        log "Directory created"
-    fi
+	if [[ ! -d "$DIR" ]]; then
+		log "Creating directory $DIR"
+		mkdir -p "$DIR"
+		log "Directory created"
+	fi
 
-    if [[ ! -f "$DIR"/"$DB" ]]; then
-        # Create the DB if not exists
-        log "Creating history database..."
-        sqlite3 "$DIR"/"$DB" <sql/watch_history_tbl.sql
-        sqlite3 "$DIR"/"$DB" <sql/search_history_tbl.sql
-        log "History database created..."
-    fi
+	if [[ ! -f "$DIR"/"$DB" ]]; then
+		# Create the DB if not exists
+		log "Creating history database..."
+		sqlite3 "$DIR"/"$DB" < sql/watch_history_tbl.sql
+		sqlite3 "$DIR"/"$DB" < sql/search_history_tbl.sql
+		log "History database created..."
+	fi
 
-    if [[ ! -f "$DIR/meh.rasi" ]]; then
-        # Move theme files and skip-intro script to correct locations
-        log "Moving theme files..."
-        cp themes/* "$DIR"/
-        log "Theme files moved..."
-    fi
+	if [[ ! -f "$DIR/meh.rasi" ]]; then
+		# Move theme files and skip-intro script to correct locations
+		log "Moving theme files..."
+		cp themes/* "$DIR"/
+		log "Theme files moved..."
+	fi
 
-    log "Creating mpv/scripts/ directory if it doesn't exist..."
-    mkdir -p "$MPV_DIR/scripts/"
-    if [[ ! -f "$MPV_DIR/scripts/skip-intro.lua" ]]; then
-      log "Moving skip-intro.lua into mpv scripts directory..."
-      cp lua/skip-intro.lua "$MPV_DIR/scripts/"
-      log "Moved skip-intro.lua into scripts directory..."
-    else
-      log "skip-intro.lua already exists in $XDG_CONFIG_HOME/mpv/scripts/"
-    fi
+	log "Creating mpv/scripts/ directory if it doesn't exist..."
+	mkdir -p "$MPV_DIR/scripts/"
+	if [[ ! -f "$MPV_DIR/scripts/skip-intro.lua" ]]; then
+		log "Moving skip-intro.lua into mpv scripts directory..."
+		cp lua/skip-intro.lua "$MPV_DIR/scripts/"
+		log "Moved skip-intro.lua into scripts directory..."
+	else
+		log "skip-intro.lua already exists in $XDG_CONFIG_HOME/mpv/scripts/"
+	fi
 
-    if [[ ! -d "$DIR/icons" ]]; then
-        log "Creating icons directory"
-        mkdir -p "$DIR/icons"
-    fi
-    # install aniwrapper icons
-    cp .assets/icons/* "$DIR/icons/"
-    log "Installed icons in config directory..."
-    # xdg-icon-resource install --size 64 ./assets/icons/icon-64.png aniwrapper --novendor
+	if [[ ! -d "$DIR/icons" ]]; then
+		log "Creating icons directory"
+		mkdir -p "$DIR/icons"
+	fi
+	# install aniwrapper icons
+	cp .assets/icons/* "$DIR/icons/"
+	log "Installed icons in config directory..."
+	# xdg-icon-resource install --size 64 ./assets/icons/icon-64.png aniwrapper --novendor
 }
 
 if run_setup; then
-    log "Setup Complete...."
+	log "Setup Complete...."
 else
-    printf "%s\n" "There was an error during setup"
-    exit 1
+	printf "%s\n" "There was an error during setup"
+	exit 1
 fi
