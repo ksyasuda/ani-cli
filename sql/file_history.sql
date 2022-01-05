7ca1907998d0d4be9761e3cb3e3ae2eff65bece3
ksyasuda <ksyasuda@umich.edu>
2022-01-04 23:06:12 -0800

    update code to insert into file history table

--- sql/file_history.sql
+++ sql/file_history.sql
@@ -1,5 +1,6 @@
 CREATE TABLE file_history (
   id integer PRIMARY KEY AUTOINCREMENT,
   directory varchar(200) NOT NULL,
-  filename varchar(200) NOT NULL
+  filename varchar(200) NOT NULL,
+  watch_date datetime NOT NULL
 );
