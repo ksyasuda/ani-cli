b3f8aa48d3267b84150babadf707fed4eee14e79
ksyasuda <ksyasuda@umich.edu>
2022-01-06 23:31:02 -0800

    update setup sql sripts to backup and update tables automatically

--- sql/file_history.sql
+++ sql/file_history.sql
@@ -1,6 +1,44 @@
-CREATE TABLE file_history (
-  id integer PRIMARY KEY AUTOINCREMENT,
-  directory varchar(200) NOT NULL,
-  filename varchar(200) NOT NULL,
-  watch_date datetime NOT NULL
+DROP TABLE IF EXISTS temp_file_history;
+
+CREATE TABLE IF NOT EXISTS temp_file_history (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    directory varchar(200) NOT NULL,
+    filename varchar(200) NOT NULL,
+    watch_date DATETIME NOT NULL,
+    CHECK (LENGTH(directory) > 0),
+    CHECK (LENGTH(filename) > 0),
+    UNIQUE (directory, filename)
 );
+
+-- make sure table has been set up at least once
+CREATE TABLE IF NOT EXISTS file_history (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    directory varchar(200) NOT NULL,
+    filename varchar(200) NOT NULL,
+    watch_date DATETIME NOT NULL,
+    CHECK (LENGTH(directory) > 0),
+    CHECK (LENGTH(filename) > 0),
+    UNIQUE (directory, filename)
+);
+
+
+INSERT OR IGNORE INTO temp_file_history
+SELECT * FROM file_history;
+
+DROP TABLE file_history;
+
+CREATE TABLE file_history (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    directory varchar(200) NOT NULL,
+    filename varchar(200) NOT NULL,
+    watch_date DATETIME NOT NULL,
+    CHECK (LENGTH(directory) > 0),
+    CHECK (LENGTH(filename) > 0),
+    UNIQUE (directory, filename)
+);
+CREATE UNIQUE INDEX dir_filename_idx ON file_history(directory, filename);
+
+INSERT INTO file_history
+SELECT * FROM temp_file_history;
+
+DROP TABLE temp_file_history;
--- sql/watch_history_tbl.sql
+++ sql/watch_history_tbl.sql
@@ -1,7 +1,41 @@
-CREATE TABLE watch_history (
-    id integer PRIMARY KEY AUTOINCREMENT,
-    anime_name varchar(200) NOT NULL,
-    episode_number integer NOT NULL,
-    watch_date datetime NOT NULL
+DROP TABLE IF EXISTS temp_watch_history;
+CREATE TABLE temp_watch_history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    anime_name VARCHAR(200) NOT NULL,
+    episode_number INTEGER NOT NULL,
+    watch_date DATETIME NOT NULL,
+    CHECK(LENGTH(anime_name) > 0),
+    CHECK(episode_number > 0),
+    UNIQUE(anime_name, episode_number, watch_date)
 );
 
+CREATE TABLE IF NOT EXISTS watch_history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    anime_name VARCHAR(200) NOT NULL,
+    episode_number INTEGER NOT NULL,
+    watch_date DATETIME NOT NULL,
+    CHECK(LENGTH(anime_name) > 0),
+    CHECK(episode_number > 0),
+    UNIQUE(anime_name, episode_number, watch_date)
+);
+
+INSERT OR IGNORE INTO temp_watch_history
+SELECT * FROM watch_history;
+
+DROP TABLE watch_history;
+
+CREATE TABLE watch_history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    anime_name VARCHAR(200) NOT NULL,
+    episode_number INTEGER NOT NULL,
+    watch_date DATETIME NOT NULL,
+    CHECK(LENGTH(anime_name) > 0),
+    CHECK(episode_number > 0),
+    UNIQUE(anime_name, episode_number, watch_date)
+);
+CREATE UNIQUE INDEX IF NOT EXISTS watch_anime_episode_idx ON watch_history(anime_name, episode_number);
+
+INSERT INTO watch_history
+SELECT * FROM temp_watch_history;
+
+DROP TABLE IF EXISTS temp_watch_history;
