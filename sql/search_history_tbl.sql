9bd2b94bda76874d122d4318a662b05d13f14d19
ksyasuda <ksyasuda@umich.edu>
2022-01-09 12:46:37 -0800

    update search table sql to not allow duplicate anime names

--- sql/search_history_tbl.sql
+++ sql/search_history_tbl.sql
@@ -1,10 +1,9 @@
-CREATE TABLE IF NOT EXISTS search_history (
+CREATE TABLE search_history (
     id integer PRIMARY KEY AUTOINCREMENT,
-    anime_name varchar(200) NOT NULL,
+    anime_name varchar(200) NOT NULL UNIQUE,
     search_date DATETIME NOT NULL,
-    CHECK (LENGTH(anime_name) > 0),
-    UNIQUE (anime_name, search_date)
+    CHECK (LENGTH(anime_name) > 0)
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS anime_search_idx ON search_history (anime_name, search_date);
+CREATE UNIQUE INDEX anime_search_idx ON search_history (anime_name, search_date);
 
